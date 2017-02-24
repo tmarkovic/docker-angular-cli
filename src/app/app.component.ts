8b6dd454a283f2e679ed35f9e1f38823d8d2bd8c
thm <theodore.markovic@payex.com>
2017-02-24 09:52:47 +0100

    added apimethods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { PeopleService } from './people-service.service';
+import { FormControl, FormGroup } from '@angular/forms';
+
+
+export class User {
+  name: string;
+  birthday: string;
+  phone: string;
+  sex: Number;
+  age: Number;
+  _id?: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,13 +19,40 @@ import { PeopleService } from './people-service.service';
 export class AppComponent implements OnInit {
   title = 'app works!';
   people: {}[];
-  sex:[] = ['Mann', 'Kvinna', 'Annat'];
-
+  sex: string[] = ['Man', 'Kvinna', 'Annat'];
+  form = new FormGroup({
+    name: new FormControl(),
+    birthday: new FormControl(),
+    phone: new FormControl(),
+    sex: new FormControl(),
+  });
   constructor(private userService: PeopleService) {
 
   }
 
   ngOnInit() {
     this.userService.getAllUsers().subscribe(res => this.people = res);
+  }
+
+  createPerson(e: Event) {
+    e.preventDefault();
+    let usr: User = new User();
+
+
+
+    usr.name = this.form.controls['name'].value;
+    usr.phone = this.form.controls['phone'].value;
+    usr.birthday = this.form.controls['birthday'].value;
+    usr.age = 23;
+    usr.sex = 3;
+
+    this.userService.createUser(usr).subscribe(res => {
+
+      this.people.push(usr);
+
+    },
+      err => {
+        console.log(err);
+      });
   }
 }
--- src/app/people-service.service.ts
+++ src/app/people-service.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
+import { User } from './app.component';
 @Injectable()
 export class PeopleService {
   API_URL: string = 'http://localhost:3000';
-
+  headers = new Headers();
   constructor(private http: Http) {
-
+    this.headers.append('Content-Type', 'application/json');
   }
 
-  getAllUsers(): Observable<{}[]> {
-    return this.http.get(`${this.API_URL}/users`).map(res => res.json());
+  getAllUsers(): Observable<User[]> {
+    return this.http.get(`${this.API_URL}/users`).map(res => <User[]>res.json());
   }
 
-
+  createUser(user: User) {
+    let payload: string = JSON.stringify(user)
+    return this.http.post(`${this.API_URL}/users`, payload, { headers: this.headers }).map(res => res.json())
+  }
 }
